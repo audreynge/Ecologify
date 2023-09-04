cb00329babc99de45a30ad27c67b77ed3e5e46da
Audrey Ng <audrey518@gmail.com>
2023-09-03 17:40:40 -0700

    started landing page

---
+++ public/script.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menuIcon = document.getElementById('menu-icon');
+//     const sideMenu = document.getElementById('side-menu');
+    
+//     // Toggle the side menu when the menu icon is clicked
+//     menuIcon.addEventListener('click', function() {
+//         sideMenu.classList.toggle('open');
+//         menuIcon.classList.toggle('open');
+//     });
+// });
